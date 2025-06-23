8708ea09474a3cb98e3d9915c1b43d24fe4f7a6b
SumeyaMolu <166501330+SumeyaMolu@users.noreply.github.com>
2025-06-23 22:52:50 +0300

    Added more details ih home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,8 +3,16 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Women Empowerment Platform</h1>
-      <p>Explore mentorship, scholarships, leadership training and more.</p>
+      <h1>Welcome to Our Empowerment Platform</h1>
+        <p>
+          We are dedicated to supporting and uplifting young women across Kenya and beyond.
+          Our platform brings together opportunities for personal and professional growth through mentorship,
+          leadership training, and scholarships.
+        </p>
+        <p>
+          We believe in the power of connection and community. By bringing together mentors, resources, and
+          learning opportunities, we aim to ensure that every girl has a chance to learn, grow, and succeed.
+        </p>
     </div>
   );
 }
