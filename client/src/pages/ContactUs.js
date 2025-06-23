29330d0ffbb7b2c00afc5192b15b8a867da60802
SumeyaMolu <166501330+SumeyaMolu@users.noreply.github.com>
2025-06-23 22:50:46 +0300

    Added a functionality

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -3,8 +3,17 @@ import React from 'react';
 function ContactUs() {
   return (
     <div className="page-content">
-      <h1>Contact Us</h1>
-      <p>If you have any questions, suggestions, or need support, feel free to reach out:</p>
+       <h1>Get in Touch With Us</h1>
+        <p>
+          We would love to hear from you! Whether you have a question about our programs, want to partner with us, or are
+          simply seeking guidance—our team is here to help. We believe communication is key to creating meaningful change
+          and are always open to feedback, collaboration, or new ideas. Don't hesitate to reach out for any support you might need.
+        </p>
+        <p>
+          You can contact us via email or phone, or even visit our offices if you're nearby. We’re committed to maintaining an
+          open channel of communication to ensure that every girl and woman who seeks help can find it. Join us in creating a
+          supportive community where women feel empowered, connected, and ready to take the next step in their journey.
+        </p>
       <ul>
         <li>Email: support@empowerwomen.ke</li>
         <li>Phone: +254 790 268 032</li>
