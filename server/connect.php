c1c7374c12ff51b10961e3ca5f7dfe1259bd5c14
SumeyaMolu <166501330+SumeyaMolu@users.noreply.github.com>
2025-06-30 11:57:41 +0300

    Made connect.php page

--- server/connect.php
+++ server/connect.php
@@ -1,12 +1,17 @@
 <?php
+// Reusable connection file
+
 $host = "localhost";
 $user = "root";
-$password = "";
-$db = "empowerher_db";
+$pass = ""; // Default is blank in XAMPP
+$db   = "empowerher_db"; // Use your database name
 
-$conn = new mysqli($host, $user, $password, $db);
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db);
 
+// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die(json_encode(['error' => 'Connection failed: ' . $conn->connect_error]));
 }
 ?>
+
