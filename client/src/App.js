1c604dd20884f166f2b17fd683c2252123dbb6b1
SumeyaMolu <166501330+SumeyaMolu@users.noreply.github.com>
2025-07-07 11:56:33 +0300

    Added changes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import MentorModule from './modules/Mentor/MentorModule';
 import ScholarshipModule from './modules/Scholarship/ScholarshipModule';
@@ -8,71 +7,24 @@ import LeadershipModule from './modules/Leadership/LeadershipModule';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
-
-function ProtectedRoute({ children }) {
-  const isRegistered = localStorage.getItem('isRegistered') === 'true';
-  return isRegistered ? children : <Navigate to="/" />;
-}
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [helloMessage, setHelloMessage] = useState('');
-  const [testMessage, setTestMessage] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/message')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error('Error fetching /api/message:', err));
-
-    fetch('http://localhost:5000/api/hello')
-      .then((res) => res.json())
-      .then((data) => setHelloMessage(data.message))
-      .catch((error) => console.error('Error fetching /api/hello:', error));
-
-    fetch('http://localhost:5000/api/test')
-      .then((res) => res.json())
-      .then((data) => setTestMessage(data.message))
-      .catch((error) => console.error('Error fetching /api/test:', error));
-  }, []);
-
   return (
     <Router>
       <div style={{ padding: '2rem' }}>
         <h1>EmpowerHer</h1>
-
-        {/* Display API messages */}
-        <div style={{ marginBottom: '1rem' }}>
-          {message && (
-            <p style={{ fontStyle: 'italic', color: '#555' }}>
-              Message from /api/message: {message}
-            </p>
-          )}
-          {helloMessage && (
-            <p style={{ fontStyle: 'italic', color: '#777' }}>
-              Message from /api/hello: {helloMessage}
-            </p>
-          )}
-          {testMessage && (
-            <p style={{ fontStyle: 'italic', color: '#999' }}>
-              Message from /api/test: {testMessage}
-            </p>
-          )}
-        </div>
-
-        {/* Navigation */}
         <nav style={{ marginBottom: '1.5rem' }}>
           <ul style={{ listStyle: 'none', display: 'flex', gap: '20px', padding: 0 }}>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About Us</Link></li>
-            <li><Link to="/mentor">Mentor's Module</Link></li>
-            <li><Link to="/scholarship">Scholarship Module</Link></li>
-            <li><Link to="/leadership">Leadership Module</Link></li>
+            <li><Link to="/mentor">Mentor</Link></li>
+            <li><Link to="/scholarship">Scholarship</Link></li>
+            <li><Link to="/leadership">Leadership</Link></li>
             <li><Link to="/contact">Contact Us</Link></li>
           </ul>
         </nav>
 
-        {/* Routing */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<ProtectedRoute><AboutUs /></ProtectedRoute>} />
@@ -87,4 +39,3 @@ function App() {
 }
 
 export default App;
-
